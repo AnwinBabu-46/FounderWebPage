ee07c0b0a8729c101e3b99c1cc3c4167919a1afb
AnwinBabu-46 <anwino2003@gmail.com>
2025-12-03 23:04:35 +0530

    fix(blog): use middleware to proxy blog requests, preventing Next.js layout from wrapping NestJS HTML

--- next.config.js
+++ next.config.js
@@ -4,18 +4,6 @@ const nextConfig = {
     domains: ['localhost'],
     unoptimized: false,
   },
-  async rewrites() {
-    return [
-      {
-        source: '/blog',
-        destination: 'http://localhost:3001/blog',
-      },
-      {
-        source: '/blog/:path*',
-        destination: 'http://localhost:3001/blog/:path*',
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
